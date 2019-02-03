0ec11c3999f182bb4beb755720cdf6a3280d509f
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-02-03 12:09:39 +0700

    Add image

--- src/templates/Post/index.js
+++ src/templates/Post/index.js
@@ -1,199 +1,206 @@
-const _ = require('lodash')
+  const _ = require('lodash')
 
-import { Link } from 'gatsby'
-import get from 'lodash/get'
-import React from 'react'
-import map from 'lodash/map'
-import Img from 'gatsby-image'
+  import { Link } from 'gatsby'
+  import get from 'lodash/get'
+  import React from 'react'
+  import map from 'lodash/map'
+  import Img from 'gatsby-image'
 
-import Adsense from 'components/Adsense'
-import Footer from 'components/Footer'
-import './style.scss'
-import 'katex/dist/katex.min.css'
+  import Adsense from 'components/Adsense'
+  import Footer from 'components/Footer'
+  import './style.scss'
+  import 'katex/dist/katex.min.css'
 
-const Post = ({ data, site, options }) => {
-  const {
-    category,
-    tags,
-    description,
-    title,
-    path,
-    links,
-    date,
-    image,
-    author,
-  } = data.frontmatter
-  const { isIndex, adsense } = options
-  const { url } = site
-  const html = get(data, 'html')
-  const isMore = isIndex && !!html.match('<!--more-->')
-  const fixed = get(image, 'childImageSharp.fixed')
-  const pageUrl = url + path
-  const relatedLinks = links || []
-  const socialTitle = title + ' | ' + site.title
+  const Post = ({ data, site, options }) => {
+    const {
+      category,
+      tags,
+      description,
+      title,
+      path,
+      links,
+      date,
+      image,
+      author,
+    } = data.frontmatter
+    const { isIndex, adsense } = options
+    const { url } = site
+    const html = get(data, 'html')
+    const isMore = isIndex && !!html.match('<!--more-->')
+    const fixed = get(image, 'childImageSharp.fixed')
+    const pageUrl = url + path
+    const relatedLinks = links || []
+    const socialTitle = title + ' | ' + site.title
 
-  return (
-    <div className="article" key={path}>
-      <div className="container">
-        <div className="info">
-          <Link style={{ boxShadow: 'none' }} to={path}>
-            <h1>{title}</h1>
-          </Link>
-          <div className="meta">
-            <div className="meta-info">
-              <time dateTime={date}>{date}</time>
-              {Badges({ items: [category], primary: true, type: 'categories' })}
-              {Badges({ items: tags, type: 'tags' })}
-            </div>
-            {Socials({ link: pageUrl, text: encodeURIComponent(socialTitle) })}
-          </div>
-        </div>
-        <div
-          className="content"
-          dangerouslySetInnerHTML={{
-            __html: isMore ? getDescription(html) : html,
-          }}
-        />
-        {((pageUrl, title) => {
-          if (isIndex == false) {
-            return (
-              <div className="bottom">
-                {Socials({ link: pageUrl, text: encodeURIComponent(socialTitle) })}
-              </div>
-            )
-          }
-        })(pageUrl, title)}
-        {(isIndex == false && relatedLinks.length > 0) ? RelatedArticles({ links: relatedLinks }) : ''}
-        {isMore ? Button({ path, label: 'MORE', primary: true }) : ''}
-        <hr />
-        {isIndex == false ? Profile({ author: author }) : ''}
-        {getAd(isIndex, adsense)}
-      </div>
-    </div>
-  )
-}
-
-export default Post
-
-const getAd = (isIndex, adsense) => {
-  return !isIndex ? <Adsense clientId={adsense} slotId="" format="auto" /> : ''
-}
-
-const getDescription = body => {
-  body = body.replace(/<blockquote>/g, '<blockquote class="blockquote">')
-  if (body.match('<!--more-->')) {
-    body = body.split('<!--more-->')
-    if (typeof body[0] !== 'undefined') {
-      return body[0]
-    }
-  }
-  return body
-}
-
-const RelatedArticles = ({ links }) => {
-  return (
-    <div className="related">
-      <h3>See also</h3>
-      <ul>
-        {(() => {
-          return map(links, (link, i) => {
-            return <li key={i}><div dangerouslySetInnerHTML={{ __html: link }} /></li>
-          })
-        })()}
-      </ul>
-    </div>
-  )
-}
-
-const Button = ({ path, label, primary }) => (
-  <Link className="readmore" to={path}>
-    <span
-      className={`btn btn-outline-primary btn-block ${
-        primary ? 'btn-outline-primary' : 'btn-outline-secondary'
-      }`}
-    >
-      {label}
-    </span>
-  </Link>
-)
-
-const Badges = ({ items, primary, type }) =>
-  map(items, (item, i) => {
     return (
-      <a
-        href={`/${type}/${_.kebabCase(item)}`}
-        className={`badge ${primary ? 'badge-primary' : 'badge-secondary'}`}
-        key={i}
-      >
-        {item}
-      </a>
+      <div className="article" key={path}>
+        <div className="container">
+          <div className="info">
+            <Link style={{ boxShadow: 'none' }} to={path}>
+              <h1>{title}</h1>
+            </Link>
+            <div className="meta">
+              <div className="meta-info">
+                <time dateTime={date}>{date}</time>
+                {Badges({ items: [category], primary: true, type: 'categories' })}
+                {Badges({ items: tags, type: 'tags' })}
+              </div>
+              {Socials({ link: pageUrl, text: encodeURIComponent(socialTitle) })}
+            </div>
+          </div>
+          <div className="content">
+            {fixed ? (
+              <Img fixed={fixed} style={{ display: 'block', margin: '0 auto 20px auto', width: '100%', height:  '300px'  }} />
+            ) : (
+              ''
+            )}
+          </div>
+          <div
+            className="content"
+            dangerouslySetInnerHTML={{
+              __html: isMore ? getDescription(html) : html,
+            }}
+          />
+          {((pageUrl, title) => {
+            if (isIndex == false) {
+              return (
+                <div className="bottom">
+                  {Socials({ link: pageUrl, text: encodeURIComponent(socialTitle) })}
+                </div>
+              )
+            }
+          })(pageUrl, title)}
+          {(isIndex == false && relatedLinks.length > 0) ? RelatedArticles({ links: relatedLinks }) : ''}
+          {isMore ? Button({ path, label: 'MORE', primary: true }) : ''}
+          <hr />
+          {isIndex == false ? Profile({ author: author }) : ''}
+          {getAd(isIndex, adsense)}
+        </div>
+      </div>
     )
-  })
+  }
 
-const Socials = ({ link, text }) => {
-  return (
-    <span className="social">
-      <a
-        href={`https://b.hatena.ne.jp/entry/s/${link.replace('https://', '')}`}
-        target="_blank"
-        className="share-btn hatena"
+  export default Post
+
+  const getAd = (isIndex, adsense) => {
+    return !isIndex ? <Adsense clientId={adsense} slotId="" format="auto" /> : ''
+  }
+
+  const getDescription = body => {
+    body = body.replace(/<blockquote>/g, '<blockquote class="blockquote">')
+    if (body.match('<!--more-->')) {
+      body = body.split('<!--more-->')
+      if (typeof body[0] !== 'undefined') {
+        return body[0]
+      }
+    }
+    return body
+  }
+
+  const RelatedArticles = ({ links }) => {
+    return (
+      <div className="related">
+        <h3>See also</h3>
+        <ul>
+          {(() => {
+            return map(links, (link, i) => {
+              return <li key={i}><div dangerouslySetInnerHTML={{ __html: link }} /></li>
+            })
+          })()}
+        </ul>
+      </div>
+    )
+  }
+
+  const Button = ({ path, label, primary }) => (
+    <Link className="readmore" to={path}>
+      <span
+        className={`btn btn-outline-primary btn-block ${
+          primary ? 'btn-outline-primary' : 'btn-outline-secondary'
+        }`}
       >
-        <i className="fa fa-hatena" />
-      </a>
-      <a
-        href={`https://twitter.com/share?url=${link}&text=${text}&hashtags=tamanyannet`}
-        target="_blank"
-        className="share-btn twitter"
-      >
-        <i className="fa fa-twitter" />
-      </a>
-      <a
-        href={`https://www.facebook.com/sharer/sharer.php?u=${link}`}
-        target="_blank"
-        className="share-btn facebook"
-      >
-        <i className="fa fa-facebook" />
-      </a>
-    </span>
+        {label}
+      </span>
+    </Link>
   )
-}
 
-const Profile = ({ author }) => (
-  <div className="profile">
-    <div className="name-icon">
-      <img className="icon" src={author.icon} />
-      <div className="name">{author.name}</div>
+  const Badges = ({ items, primary, type }) =>
+    map(items, (item, i) => {
+      return (
+        <a
+          href={`/${type}/${_.kebabCase(item)}`}
+          className={`badge ${primary ? 'badge-primary' : 'badge-secondary'}`}
+          key={i}
+        >
+          {item}
+        </a>
+      )
+    })
+
+  const Socials = ({ link, text }) => {
+    return (
+      <span className="social">
+        <a
+          href={`https://b.hatena.ne.jp/entry/s/${link.replace('https://', '')}`}
+          target="_blank"
+          className="share-btn hatena"
+        >
+          <i className="fa fa-hatena" />
+        </a>
+        <a
+          href={`https://twitter.com/share?url=${link}&text=${text}&hashtags=tamanyannet`}
+          target="_blank"
+          className="share-btn twitter"
+        >
+          <i className="fa fa-twitter" />
+        </a>
+        <a
+          href={`https://www.facebook.com/sharer/sharer.php?u=${link}`}
+          target="_blank"
+          className="share-btn facebook"
+        >
+          <i className="fa fa-facebook" />
+        </a>
+      </span>
+    )
+  }
+
+  const Profile = ({ author }) => (
+    <div className="profile">
+      <div className="name-icon">
+        <img className="icon" src={author.icon} />
+        <div className="name">{author.name}</div>
+      </div>
+      <div className="social-accounts">
+        <a
+          href={`https://twitter.com/tamanyan55`}
+          target="_blank"
+          className="share-btn twitter"
+        >
+          <i className="fa fa-twitter" />
+        </a>
+        <a
+          href={`https://github.com/tamanyan`}
+          target="_blank"
+          className="share-btn github"
+        >
+          <i className="fa fa-github" />
+        </a>
+        <a
+          href={`https://www.linkedin.com/in/tyoshida`}
+          target="_blank"
+          className="share-btn linkedin"
+        >
+          <i className="fa fa-linkedin" />
+        </a>
+        <a
+          href={`mailto:tamanyan.ttt@gmail.com`}
+          target="_blank"
+          className="share-btn email"
+        >
+          <i className="fa fa-email" />
+        </a>
+      </div>
+      <div className="description">{author.bio}</div>
     </div>
-    <div className="social-accounts">
-      <a
-        href={`https://twitter.com/tamanyan55`}
-        target="_blank"
-        className="share-btn twitter"
-      >
-        <i className="fa fa-twitter" />
-      </a>
-      <a
-        href={`https://github.com/tamanyan`}
-        target="_blank"
-        className="share-btn github"
-      >
-        <i className="fa fa-github" />
-      </a>
-      <a
-        href={`https://www.linkedin.com/in/tyoshida`}
-        target="_blank"
-        className="share-btn linkedin"
-      >
-        <i className="fa fa-linkedin" />
-      </a>
-      <a
-        href={`mailto:tamanyan.ttt@gmail.com`}
-        target="_blank"
-        className="share-btn email"
-      >
-        <i className="fa fa-email" />
-      </a>
-    </div>
-    <div className="description">{author.bio}</div>
-  </div>
-)
+  )
