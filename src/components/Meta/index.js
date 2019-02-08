9fff3cd5eab629dfeb040ba7efce93f63dfbf154
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-02-08 11:57:42 +0700

    Add tamanyan_me.png

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -5,7 +5,7 @@ import get from 'lodash/get'
 const Meta = ({ site, title, url, image, description }) => {
   const metaTitle = title || get(site, 'title')
   const metaDescription = description || get(site, 'description')
-  const metaImage = image || `${get(site, 'url')}/img/tamanyan_net.jpg`
+  const metaImage = image || `${get(site, 'url')}/img/tamanyan_me.png`
   const metaUrl = url || get(site, 'url')
 
   return (
